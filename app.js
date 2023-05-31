10dce91f1d00f1a39b2d40a1a9866ff1ae969a22
Nolasquito <nolascopalavecino.com>
2023-05-31 07:15:41 -0300

    Resgister sin el js

---
+++ app.js
@@ -0,0 +1,4 @@
+const name = document.getElementById("InputName");
+const apellido = document.getElementById("InputApellido");
+const email = document.getElementById("InputEmail");
+const pass = document.getElementById("InputPassword");
