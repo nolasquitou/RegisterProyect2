a80fa93fc8983ed48bb0aeed968c2d5b2657a91d
Nolasquito <nolascopalavecino.com>
2023-05-31 16:22:46 -0300

    register con las validaciones JS

--- app.js
+++ app.js
@@ -1,4 +1,63 @@
-const name = document.getElementById("InputName");
-const apellido = document.getElementById("InputApellido");
-const email = document.getElementById("InputEmail");
-const pass = document.getElementById("InputPassword");
+const nameHTML = document.getElementById("InputName");
+const apellidoHTML = document.getElementById("InputApellido");
+const emailHTML = document.getElementById("InputEmail");
+const passHTML = document.getElementById("InputPassword");
+const textInfo = document.getElementById("textInfo")
+
+function regiterHandler(e) {
+    e.preventDefault()
+    const expreEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    let warnings = ""
+    let verific = false
+    const datos = {
+        Nombre: nameHTML.value,
+        Apellido: apellidoHTML.value,
+        Email: emailHTML.value,
+        Contraseña: passHTML.value
+    }
+    console.log(datos);
+    for (const key in datos) {
+        const element = datos[key].trim();
+
+        if (element == "") {
+            warnings += key + " esta vacio" + "<br>"
+            verific = true
+            if (verific) {
+                textInfo.innerHTML = warnings
+            }
+        } else {
+            if (key == "Nombre") {
+                if (nameHTML.value.length < 4) {
+                    warnings += `El ombre es muy corto <br>`
+                    verific = true
+                }
+            }
+            if (key == "Nombre") {
+                if (nameHTML.value.length < 4) {
+                    warnings += `El ombre es muy corto <br>`
+                    verific = true
+                }
+            }
+            if (key == "Email") {
+                console.log(element);
+                if (expreEmail.test(emailHTML.value)) {
+                    console.log("email si");
+                } else {
+                    warnings += `El Email no es validp <br>`
+                    verific = true
+                }
+            }
+            if (key == "Contraseña") {
+                if (passHTML.value.length < 6) {
+                    warnings += `La Contraseña es muy corta <br>`
+                    verific = true
+                }
+            }
+
+            if (verific) {
+                textInfo.innerHTML = warnings
+            }
+        }
+
+    }
+}
